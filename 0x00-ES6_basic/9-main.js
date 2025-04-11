df075b9a49cbef7ca71df761dfe6f9576555e679
IamSila <silamulingi@gmail.com>
2025-04-11 17:47:03 +0300

    mehod properties in js

--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -2,5 +2,5 @@ import getFullBudgetObject from './9-getFullBudget';
 
 const fullBudget = getFullBudgetObject(20, 50, 10);
 
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeIndollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
