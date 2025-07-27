986c56c49e8845a4fa6881c2e38179d6528c6a9a
IamSila <silamulingi@gmail.com>
2025-07-27 23:09:32 +0300

    1-promises.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,6 @@
-import { get } from "mongoose";
-
 export default function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-        if (success === true) {
+        if (success) {
             resolve({
                 status: 200,
                 body: 'Success'
